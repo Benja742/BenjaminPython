7e7ea0eb6eaddaea919f3b732c6e17db74bbcafa
benja742 <b3njaminandres@gmail.com>
2026-04-13 09:45:56 -0400

    ejemplos de variables

--- prueba.py
+++ prueba.py
@@ -1,1 +1,20 @@
 print("Hola mundo") 
+
+# creando variables
+
+titulo="Clima de hoy"  # String
+diaDelMes=13  # Int
+mes=4  # Int
+
+temperatura=22.3  # float
+
+llueve=False # boolean
+
+print(titulo)
+print("Temperatura actual:", temperatura, "grados")
+print(diaDelMes, "-", mes)
+
+if llueve:
+    print("Tiene que llevar paraguas")
+else:
+    print("Puede llevar polera sin mangas")
